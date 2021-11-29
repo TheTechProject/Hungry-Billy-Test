b0e545a7444effaf2d77e0d84ad0966e0276ce7b
Sam Harris <SH248212@falmouth.ac.uk>
2021-11-29 23:48:25 +0000

    Added ability to add rovers from another script

--- Hungry-Billy/Assets/Scripts/Enemies/RoverDetector.cs
+++ Hungry-Billy/Assets/Scripts/Enemies/RoverDetector.cs
@@ -6,9 +6,29 @@ public class RoverDetector : MonoBehaviour
 {
     [SerializeField] private Enemy[] enemiesInRange;
 
+    /// <summary>
+    /// Adds the cannons on the platform of the detector
+    /// for automating firing.
+    /// </summary>
+    /// <param name="enemies"></param>
+    public void AddEnemyToList(Enemy[] enemies)
+    {
+        enemiesInRange = new Enemy[enemies.Length];
+        int count = 0;
+        foreach (var enemy in enemies)
+        {
+            enemiesInRange[count] = enemy;
+            count++;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Rover")
+        /*
+         * When a rover enters the detectors trigger it
+         * will add them as targets to the cannon.
+         */
+        if (other.gameObject.tag == "Rover")
         {
             foreach(var enemy in enemiesInRange)
             {
