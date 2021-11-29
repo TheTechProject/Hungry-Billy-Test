0f222dd2c4765f7b47ced73a2e30d580c27ad271
Sam Harris <SH248212@falmouth.ac.uk>
2021-11-29 23:48:09 +0000

    Commented and added animation

--- Hungry-Billy/Assets/Scripts/Enemies/Enemy.cs
+++ Hungry-Billy/Assets/Scripts/Enemies/Enemy.cs
@@ -6,30 +6,54 @@ public class Enemy : MonoBehaviour
 {
     private int health = 100;
 
+    // The rover the turret is aiming for
     private GameObject currentTarget;
 
+    // Used for firing animation
+    private Animation animator;
+
+    // Rovers that passed the entry detector for the current section
     List<GameObject> targetsInRange = new List<GameObject>();
 
     [SerializeField] private float attackCooldown = 2.0f;
-    private float attackTimer;
+    private float attackTimer; // Counts up to the attack Cooldown
 
+    /// <summary>
+    /// Taking damage from rover
+    /// </summary>
+    /// <param name="damage"></param>
     public void TakeDamage(int damage)
     {
         health -= damage;
     }
 
+    /// <summary>
+    /// Runs when a rover passes the entry detector. Adds them
+    /// as a target to the turret.
+    /// </summary>
+    /// <param name="target"></param>
     public void AddNewTarget(GameObject target)
     {
         targetsInRange.Add(target);
         Debug.Log("Target Entered Area");
     }
 
+    /// <summary>
+    /// Runs when a rover passes the exit detector. Removes them as
+    /// a target to the turret.
+    /// </summary>
+    /// <param name="target"></param>
     public void RemoveTarget(GameObject target)
     {
         targetsInRange.Remove(target);
         Debug.Log("Target Exited Area");
     }
 
+    /// <summary>
+    /// Attacks the current rover it is targeting after each cooldown interval.
+    /// if the rover it's targeting is destroyed it will target another one
+    /// in its sector.
+    /// </summary>
     private void AttackTarget()
     {
         currentTarget = null;
@@ -44,6 +68,7 @@ public class Enemy : MonoBehaviour
 
         if(attackTimer >= attackCooldown && currentTarget != null)
         {
+            //animator.Play();
             currentTarget.GetComponent<Rover>().Damage(25);
             attackTimer = 0.0f;
         }
@@ -52,7 +77,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animation>();
     }
 
     // Update is called once per frame
